3e6bf1195bc48da91b2d0e4cdba813cf783de556
Future-2023 <127534411+kostiantyn98x@users.noreply.github.com>
2022-12-18 03:03:21 +0200

    fix ssl and other file

--- config/mySql.js
+++ config/mySql.js
@@ -1,10 +1,4 @@
 const mysql = require("mysql");
 
-var connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "opensea"
-});
 
 module.exports = connection;
--- routes/api/api.js
+++ routes/api/api.js
@@ -1,10 +1,2 @@
 const sql = require("../../config/mySql");
 
-exports.userQuery = function(adminEmail) {
-  return new Promise(function (resolve, reject) {
-    sql.query(`SELECT * FROM admins where adminEmail = '${adminEmail}'`, function (err, results, fields) {
-      if (err) return reject(err);
-      return resolve(results);
-    });
-  });
-};
